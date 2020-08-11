2845d12e96f1ee039c03af0ef62285a95415b145
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2020-08-11 20:32:17 +0800

    shell: fzf key bindings

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */Users/alacritas/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/alacritas/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/alacritas/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+source "/Users/alacritas/.fzf/shell/key-bindings.bash"
